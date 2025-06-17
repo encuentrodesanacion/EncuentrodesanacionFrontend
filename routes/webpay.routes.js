e1d180944b725c4ddfa4fbf099b77eb128771879
Spaholistico <spaholistico@encuentrodesanacion.com>
2025-06-17 19:57:03 -0400

    fix:oye

--- routes/webpay.routes.js
+++ routes/webpay.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const webpayController = require("../controllers/webpayController"); // Asegúrate de que la ruta sea correcta
 
 // Ruta POST para iniciar la transacción de Webpay
-router.post("/", webpayController.crearTransaccionInicial);
+router.post("/create-transaction", webpayController.crearTransaccionInicial);
 
 // Ruta para la confirmación de Webpay.
 // Aceptará tanto POST (de Transbank) como GET (de la redirección del navegador).
